7258a075420dbcafae6bef59dcf4140654e2cedb
marilupacheco-blip <246512483+marilupacheco-blip@users.noreply.github.com>
2026-04-16 17:57:54 -0600

    Save&Load

--- Kimil-Kinich/Assets/Nodos/Save.cs
+++ Kimil-Kinich/Assets/Nodos/Save.cs
@@ -11,20 +11,24 @@ using static UnityEngine.Rendering.DebugUI;
 
 public class Save : MonoBehaviour
 {
-    int newbank;
     public int banco;
+    public float salud;
+    public int contador;
+    public int vida;
     [SerializeField] TextMeshProUGUI Bank;
+    [SerializeField] TextMeshProUGUI Health;
+    [SerializeField] TextMeshProUGUI Enemy_Death;
+    [SerializeField] TextMeshProUGUI Lives;
 
-
-    
 
     public void Update()
     {
 
         // banco = banco + Variables.Scene(SceneManager.GetActiveScene()).Get<int>("Money");
         Bank.text = "Money:" + banco.ToString();
-
-
+        Health.text = "Health:" + salud.ToString();
+        Enemy_Death.text = "Enemies Killed:" + contador.ToString();
+        Lives.text = "Lives:" + vida.ToString();
     }
 
 
@@ -35,7 +39,22 @@ public class Save : MonoBehaviour
             banco = Variables.Scene(SceneManager.GetActiveScene()).Get<int>("Money");
             Debug.Log(banco);
             PlayerPrefs.SetInt("Banko", banco);
-            PlayerPrefs.Save();
+            
+            salud = Variables.Scene(SceneManager.GetActiveScene()).Get<float>("Health_Player");
+            Debug.Log(salud);
+            PlayerPrefs.SetFloat("Salud",salud);
+
+            contador = Variables.Scene(SceneManager.GetActiveScene()).Get<int>("Counter");
+            Debug.Log(contador);
+            PlayerPrefs.SetInt("Contador",contador);
+
+            vida = Variables.Scene(SceneManager.GetActiveScene()).Get<int>("Health_Player");
+            Debug.Log(vida);
+            PlayerPrefs.SetInt("Vida", vida);
+
+
+
+        PlayerPrefs.Save();
         } 
 
     public void LoadScore()
@@ -44,6 +63,18 @@ public class Save : MonoBehaviour
         banco = PlayerPrefs.GetInt("Banko");
         Variables.Scene(SceneManager.GetActiveScene()).Set("Money", banco);
         Debug.Log(banco);
+
+        salud = PlayerPrefs.GetFloat("Salud");
+        Variables.Scene(SceneManager.GetActiveScene()).Set("Health_Player", salud);
+        Debug.Log(salud);
+
+        contador = PlayerPrefs.GetInt("Contador");
+        Variables.Scene(SceneManager.GetActiveScene()).Set("Counter", contador);
+        Debug.Log(contador);
+
+        vida = PlayerPrefs.GetInt("Vida");
+        Variables.Scene(SceneManager.GetActiveScene()).Set("Health_Player", vida);
+        Debug.Log(vida);
     }
 
     public void DeleteData()
